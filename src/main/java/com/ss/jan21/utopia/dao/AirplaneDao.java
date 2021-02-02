75589ebc03ae47db89b68277aa0b3805c1804df3
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-02-01 22:25:16 -0600

    Created airplane DAO

--- src/main/java/com/ss/jan21/utopia/dao/AirplaneDao.java
+++ src/main/java/com/ss/jan21/utopia/dao/AirplaneDao.java
@@ -1,4 +1,15 @@
-package com.ss.springboot.dao;
+package com.ss.jan21.utopia.dao;
 
+import com.ss.jan21.utopia.model.Airplane;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AirplaneDao {
+
+    public Airplane getAirplaneById(int id) {
+        Airplane airplane = new Airplane();
+        airplane.setAirplaneId(id);
+        airplane.setAirplaneType("type"+id);
+        return null;
+    }
 }
