d336bd40edd669bd6054c360b0dc09bc70294ce8
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-02-01 22:41:39 -0600

    Added route to controller

--- src/main/java/com/ss/jan21/utopia/controller/AirplaineController.java
+++ src/main/java/com/ss/jan21/utopia/controller/AirplaineController.java
@@ -1,6 +1,9 @@
 package com.ss.jan21.utopia.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ss.jan21.utopia.model.Airplane;
 import com.ss.jan21.utopia.service.AirplaneService;
@@ -11,8 +14,9 @@ public class AirplaineController {
     @Autowired
     AirplaneService airplaneService;
 
-    public Airplane getAirplaneById(int id) {
-        return airplaneService.getAirplaneById(id);
+    @RequestMapping(method = RequestMethod.GET, path = "/lms/airplanes/{airplaneId}")
+    public Airplane getAirplaneById(@PathVariable int airplaneId) {
+        return airplaneService.getAirplaneById(airplaneId);
     }
 
 }
