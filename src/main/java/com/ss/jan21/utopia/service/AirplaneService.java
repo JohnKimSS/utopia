9e34460ff2d4195aa5d3f8232ca63eda40cd09c7
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-02-02 14:58:36 -0600

    Added services for corresponding routes in controller
    getAllAirplanes
    createAirplanes

--- src/main/java/com/ss/jan21/utopia/service/AirplaneService.java
+++ src/main/java/com/ss/jan21/utopia/service/AirplaneService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.ss.jan21.utopia.model.Airplane;
 import com.ss.jan21.utopia.dao.AirplaneDao;
+import java.sql.SQLException;
+import java.util.List;
 
 @Component
 public class AirplaneService {
@@ -11,8 +13,15 @@ public class AirplaneService {
     @Autowired
     AirplaneDao airplaneDao;
 
-    public Airplane getAirplaneById(int airplaneId) {
+    public Airplane getAirplaneById(int airplaneId) throws ClassNotFoundException, SQLException {
         return airplaneDao.getAirplaneById(airplaneId);
     }
 
+    public List<Airplane> getAllAirplanes() throws SQLException, ClassNotFoundException {
+        return airplaneDao.getAllAirplanes();
+    }
+
+    public void createAirplane(Airplane airplane) throws SQLException, ClassNotFoundException {
+        airplaneDao.createAirplane(airplane);
+    }
 }
