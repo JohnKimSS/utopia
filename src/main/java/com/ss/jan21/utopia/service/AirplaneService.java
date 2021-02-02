09f4a19b05dab1eec1ac08544ba52988963d1d0b
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-02-01 22:25:27 -0600

    Created Airplane service

--- src/main/java/com/ss/jan21/utopia/service/AirplaneService.java
+++ src/main/java/com/ss/jan21/utopia/service/AirplaneService.java
@@ -1,4 +1,18 @@
-package com.ss.springboot.service;
+package com.ss.jan21.utopia.service;
 
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.ss.jan21.utopia.model.Airplane;
+import com.ss.jan21.utopia.dao.AirplaneDao;
+
+@Component
 public class AirplaneService {
+
+    @Autowired
+    AirplaneDao airplaneDao;
+
+    public Airplane getAirplaneById(int id) {
+        return airplaneDao.getAirplaneById(id);
+    }
+
 }
