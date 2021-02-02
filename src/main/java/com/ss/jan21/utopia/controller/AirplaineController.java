7d0f621eb70b76221a9b2e99ff55ec96ab0cdf5a
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-02-01 22:24:50 -0600

    Created API controller for airplanes

--- src/main/java/com/ss/jan21/utopia/controller/AirplaineController.java
+++ src/main/java/com/ss/jan21/utopia/controller/AirplaineController.java
@@ -1,4 +1,18 @@
-package com.ss.springboot.controller;
+package com.ss.jan21.utopia.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import com.ss.jan21.utopia.model.Airplane;
+import com.ss.jan21.utopia.service.AirplaneService;
+
+@RestController
 public class AirplaineController {
+
+    @Autowired
+    AirplaneService airplaneService;
+
+    public Airplane getAirplaneById(int id) {
+        return airplaneService.getAirplaneById(id);
+    }
+
 }
