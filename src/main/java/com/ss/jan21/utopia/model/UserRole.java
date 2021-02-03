e65b41d9414f2f3629e568ff3a4fdbd9b749ef00
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-02-02 21:05:54 -0600

    Created UserRole model

--- src/main/java/com/ss/jan21/utopia/model/UserRole.java
+++ src/main/java/com/ss/jan21/utopia/model/UserRole.java
@@ -1,9 +1,20 @@
 package com.ss.jan21.utopia.model;
 
+import java.util.List;
+
 public class UserRole {
 
     int roleId;
     String roleName;
+    List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 
     public UserRole(int roleId, String roleName) {
         this.roleId = roleId;
